456390144c93b5cf0241ab9574f0615ab6829b50
Sumit S <45414360+sumitnair001@users.noreply.github.com>
2018-12-27 13:06:48 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "bucketName" {
+    type = "string"
+}
+
+variable "std_to_IA" {
+  type = "string" 
+  
+}
+
+variable "IA_to_glacier" {
+  type = "string"
+  
+}
+
+variable "expdays" {
+  type = "string"
+  
+}
+
